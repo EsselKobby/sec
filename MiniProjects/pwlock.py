fe4a327ee01c8cb09cdfa451ea0273a53fdcbc69
Bernard Essel <108433679+EsselKobby@users.noreply.github.com>
2024-09-30 00:19:03 +0000

    Delete MiniProjects/pwlock.py

--- MiniProjects/pwlock.py
+++
@@ -1,8 +0,0 @@
-#! python3
-# pwlock.py - A password locker program.
-
-#Program Design and Data Structures
-PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
-             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
-             'luggage': '12345'} 
-
